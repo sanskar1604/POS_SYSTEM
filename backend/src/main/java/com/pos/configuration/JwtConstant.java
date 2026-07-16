6fd104d8f32cb2f801329241a845a499658454bf
sanskar1604 <167sanskargoyal@gmail.com>
2026-07-16 13:33:34 +0530

    second commit

--- backend/src/main/java/com/pos/configuration/JwtConstant.java
+++ backend/src/main/java/com/pos/configuration/JwtConstant.java
@@ -2,6 +2,6 @@ package com.pos.configuration;
 
 public class JwtConstant {
 
-	public static final String JWT_SECRET = "bhjagfaeygfeshbfhrwbgribkbsdkBvbkrjhGIURHKJVKjbkvjbHGRUHRFskjBJRKBFK";
+	public static final String JWT_SECRET = "bhjagfaeygfeshbfhrwbgribkbsdkBvbkrjhGIURHKJVKjbkvjbHGRUHRFskjBJRKBFKdsgdgfkadoyal";
 	public static final String JWT_HEADER = "Authorization";
 }
